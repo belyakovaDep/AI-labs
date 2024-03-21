69d42570a8567ac53e411aeecc7e3ed7279e0325
d.belyakova1 <d.belyakova1@g.nsu.ru>
2024-03-21 19:36:30 +0700

    add augm

--- classification/test.py
+++ classification/test.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision as tv
 import torchvision.transforms as tf
 import multiprocessing
+import random
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
@@ -12,8 +13,8 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     transform = tf.Compose([tf.Resize(256), tf.ToTensor(),
-                            tf.Normalize(mean = [0.5, 0.5, 0.5], std = [0.225, 0.225, 0.225]), 
-                            tf.CenterCrop(224)])
+                        tf.Normalize(mean = [0.5, 0.5, 0.5], std = [0.225, 0.225, 0.225]), 
+                        tf.CenterCrop(224), random.choice([tf.RandomHorizontalFlip(), tf.RandomRotation(degrees=15)]) ])
 
     orgset = tv.datasets.ImageFolder('./simpsons_dataset', transform = transform)
 
